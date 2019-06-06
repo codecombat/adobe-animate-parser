f3407e51859527912063ba3b6cc8c5c90e11d25b
Joe Mifsud <joe@mifsud.me>
2019-06-05 17:30:15 -0700

    Remove top level self reference during translation

--- index.js
+++ index.js
@@ -12,7 +12,15 @@ lineTween(cjs, animate)
 const lib = animate.cocoGetComposition().getLibrary()
 const dataTree = new lib['linetween_HTML5']()
 
-let parsedTree = parseMovieClip(dataTree)
+let parsedMovieClip = parseMovieClip(dataTree)
+
+// For now assume top level is a movie clip and the top level has
+// a self referencing tween that we do not support.
+// We assume this self referencing tween is always the first tween.
+parsedMovieClip.tweens = parsedMovieClip.tweens.filter((tween) => {
+    // TODO remove direct _cocoId reference here
+    return parsedMovieClip._cocoId !== tween.target.reference
+})
 
 const outputSchema = translate(schema)
 console.log(JSON.stringify(outputSchema))
