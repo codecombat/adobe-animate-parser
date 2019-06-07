d01baa134eb1f22a0c0cf51fe2eb0d05effe0bf6
Joe Mifsud <joe@mifsud.me>
2019-06-07 15:20:42 -0700

    Pick proper entrypoint

--- index.js
+++ index.js
@@ -1,20 +1,71 @@
-import cjs from './lib/cjs'
+import fs from 'fs'
+
+import createjs from './lib/cjs'
 import Animate from './lib/animation'
 
-import lineTween from './fixtures/line-tween/line-tween_HTML5'
-import ninjaWalk from './fixtures/ninjawalk/NinjaWalk_animate'
-import stateTween from './fixtures/state_tween/stateTween'
+// import lineTween from './fixtures/line-tween/line-tween_HTML5'
+// import ninjaWalk from './fixtures/ninjawalk/NinjaWalk_animate'
+// import stateTween from './fixtures/state_tween/stateTween'
 
 import { parseMovieClip, targetCache, schema } from './lib/parse'
 import translate from './lib/translate'
 
-const animate = new Animate()
-stateTween(cjs, animate)
+const cjs = createjs
+let animate
 
-const lib = animate.cocoGetComposition().getLibrary()
-const dataTree = new lib['linetween_HTML5']()
+const importedFile = fs.readFileSync('./fixtures/ninjawalk/NinjaWalk_animate.js')
+const execution = `
+    createjs = cjs;
+    ${importedFile};
+    
+    animate = AdobeAn;
+`
 
-let parsedMovieClip = parseMovieClip(dataTree)
+eval(execution)
+
+const animateCompositions = animate.compositions || {}
+const compositionKeys = Object.keys(animateCompositions)
+
+if (compositionKeys.length !== 1) {
+    throw new Error('Unexpected number of compositions')
+}
+
+const library = animateCompositions[compositionKeys[0]].getLibrary()
+
+const keyCounts = {}
+const movieClips = {}
+
+for (const [ key, value ] of Object.entries(library)) {
+    if (value.prototype instanceof cjs.MovieClip) {
+        const usageTrackingFn = function (...args) {
+            keyCounts[key] = keyCounts[key] || 0
+            keyCounts[key] += 1
+
+            return value.call(this, ...args)
+        }
+
+        usageTrackingFn.prototype = value.prototype
+
+        library[key] = usageTrackingFn
+        movieClips[key] = usageTrackingFn
+    }
+}
+
+for (const [ key , value ] of Object.entries(movieClips)) {
+    movieClips[key] = new value()
+}
+
+let minKey
+let minCount = Infinity
+
+for (const [ key , value ] of Object.entries(keyCounts)) {
+    if (value < minCount) {
+        minCount = value
+        minKey = key
+    }
+}
+
+let parsedMovieClip = parseMovieClip(movieClips[minKey])
 
 // For now assume top level is a movie clip and the top level has
 // a self referencing tween that we do not support.
