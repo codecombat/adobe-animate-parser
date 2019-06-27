6d391629bb46b232897ce6bf8efc029e4def0441
Joe Mifsud <joe@mifsud.me>
2019-06-27 10:29:17 -0700

    Fix shared cocoSchema across objects of the same type

--- src/lib/cjs/CocoSchema.js
+++ src/lib/cjs/CocoSchema.js
@@ -1,7 +1,11 @@
 export default class CocoSchema {
   get cocoSchema () {
-    if (!this._cocoSchema) {
-      this._cocoSchema = {}
+    // Because of the way CreateJS does extends its objects / constructs prototypes we need to
+    // handle _cocoSchema definitions in a non standard way.  Doing a simple (if _cocoSchema) check
+    // will search the prototype chain and end up in a shared cocoSchema amongst objects of the
+    // same type
+    if (!this.hasOwnProperty('_cocoSchema')) {
+      Object.defineProperty(this, '_cocoSchema', { value: {} })
     }
 
     return this._cocoSchema
