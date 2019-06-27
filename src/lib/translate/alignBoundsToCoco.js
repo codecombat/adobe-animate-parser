5396ae8cec4c007478e57e937e6f6252425e3af4
Joe Mifsud <joe@mifsud.me>
2019-06-27 11:09:07 -0700

    Update width and height of top level bounding box based on stage bounds.

--- src/lib/translate/alignBoundsToCoco.js
+++ src/lib/translate/alignBoundsToCoco.js
@@ -1,37 +1,30 @@
+/**
+ * Reset the bounds of the top level animation to so that the game engine
+ * properly understands the bounding box of the asset.
+ *
+ * This requires that all animations be exported from Adobe Animate with
+ * the stage bounds equal to the width and height of the bounding box of
+ * the asset.
+ */
 export default function alignBoundsToCoco (schema) {
   const {
     width,
     height
   } = schema.properties
 
-  if (typeof width !== 'undefined' && typeof height !== 'undefined') {
-    const quarterWidth = width / 4
-    const quarterHeight = height / 4
+  const entryPoint = schema
+    .animations
+    .find(a => a.id === schema.entryPointName)
 
-    const entryPoint = schema
-      .animations
-      .find(a => a.id === schema.entryPointName)
+  const {
+    bounds: boundsNode
+  } = entryPoint.data
 
-    const {
-      bounds: boundsNode,
-      frameBounds: frameBoundsNodes
-    } = entryPoint.data
-
-    const resolvedBoundsNode = boundsNode.node
-    if (resolvedBoundsNode.data.length > 0) {
-      resolvedBoundsNode.data[0] -= quarterWidth
-      resolvedBoundsNode.data[1] -= quarterHeight
-    }
-
-    if (frameBoundsNodes && frameBoundsNodes.length > 0) {
-      for (const frameBoundNode of frameBoundsNodes) {
-        const resolvedFrameBoundNode = frameBoundNode.node
-
-        resolvedFrameBoundNode.data[0] -= quarterWidth
-        resolvedFrameBoundNode.data[1] -= quarterHeight
-      }
-    }
-  }
+  const resolvedBoundsNode = boundsNode.node
+  resolvedBoundsNode.data = [
+    0, 0,
+    width, height
+  ]
 
   return schema
 }
